cdc13f89dc6886afcb2c979408e62dc7a67ca923
Valep42 <p.czempin@gmx.de>
2014-07-27 20:24:59 +0200

    added ios src folder

---
+++ ios/src/com/badlogicgames/plane/IOSLauncher.java
@@ -0,0 +1,23 @@
+package com.badlogicgames.plane;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import towerDefense.StartGame;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+	@Override
+	protected IOSApplication createApplication() {
+		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+		return new IOSApplication(new StartGame(), config);
+	}
+
+	public static void main(String[] argv) {
+		NSAutoreleasePool pool = new NSAutoreleasePool();
+		UIApplication.main(argv, null, IOSLauncher.class);
+		pool.close();
+	}
+}
