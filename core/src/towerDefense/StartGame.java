3ab4d74a488df2e0a6c2f6f28130a3bfb59e35a2
Valep42 <p.czempin@gmx.de>
2014-08-02 23:09:51 +0200

    showing fps in title

--- core/src/towerDefense/StartGame.java
+++ core/src/towerDefense/StartGame.java
@@ -30,6 +30,7 @@ public class StartGame extends ApplicationAdapter {
 
 	@Override
 	public void render() {
+		Gdx.graphics.setTitle("" + Gdx.graphics.getFramesPerSecond());
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		//
