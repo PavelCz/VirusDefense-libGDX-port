0b5715a050847be57038a8c86f22b37f606642ec
Valep42 <p.czempin@gmx.de>
2014-08-03 14:22:34 +0200

    made window not resizable

--- desktop/src/desktop/DesktopLauncher.java
+++ desktop/src/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1024;
 		config.height = 768;
+		config.resizable = false;
 		new LwjglApplication(new StartGame(), config);
 	}
 }
