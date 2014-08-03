608bef6691e3e134179a6f8a0659c571bb935752
Valep42 <p.czempin@gmx.de>
2014-08-03 20:01:43 +0200

    reverted reading resolution from file

--- desktop/src/desktop/DesktopLauncher.java
+++ desktop/src/desktop/DesktopLauncher.java
@@ -2,8 +2,6 @@ package desktop;
 
 import towerDefense.StartGame;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -11,18 +9,8 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-
-		Preferences prefs = Gdx.app.getPreferences("VirusDefense");
-		String resolution = prefs.getString("resolution");
-		String[] parts = resolution.split("\n");
-		config.width = Integer.parseInt(parts[0]);
-		config.height = Integer.parseInt(parts[1]);
-		if (Integer.parseInt(parts[2]) == 1) {
-
-			config.fullscreen = true;
-		} else {
-			config.fullscreen = false;
-		}
+		config.width = 1024;
+		config.height = 768;
 		config.resizable = false;
 		new LwjglApplication(new StartGame(), config);
 	}
