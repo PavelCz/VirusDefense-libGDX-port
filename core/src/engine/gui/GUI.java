dd5e1e008b323d42fe65e1e082a81a0f1e1886c1
Valep42 <p.czempin@gmx.de>
2015-05-10 19:10:57 +0200

    tried to fix more y positions

--- core/src/engine/gui/GUI.java
+++ core/src/engine/gui/GUI.java
@@ -1,6 +1,7 @@
 package engine.gui;
 
 import towerDefense.Gameplay;
+import towerDefense.TowerDefense;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -15,6 +16,13 @@ public abstract class GUI {
 	}
 
 	protected void draw(RenderObject renderObject, SpriteBatch batch) {
+		float y = this.y;
+
+		y = TowerDefense.getHeight() - y; // sets coordinate System from up - right to down - right
+
+		// y = this.y - this.height * scaling; // sets picture anchor to top left corner instead of bottom left
+
+		y = this.y + Gameplay.DEFAULT_SIZE * Gameplay.CURRENT_GAME_SCALE;
 		renderObject.draw(this.x, this.y, Gameplay.GLOBAL_GUI_SCALE, batch);
 
 	}
