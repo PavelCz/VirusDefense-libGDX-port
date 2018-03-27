19b0f65899e00b8641531032d4d53b323202d7ff
Pavel C <valep.pave@gmx.de>
2018-03-28 00:35:24 +0200

    Remove StaticText from Scores

--- core/src/towerDefense/Scores.java
+++ core/src/towerDefense/Scores.java
@@ -4,12 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 import engine.GameComponent;
 import engine.gui.SetGameModeAction;
-import engine.gui.StaticText;
 
 public class Scores extends GameComponent {
 
@@ -42,10 +42,12 @@ public class Scores extends GameComponent {
 			String[] parts = scoresList[i].split(", ");
 			scoreString += (i + 1) + ": " + parts[0] + ", " + parts[1] + " Punkte\n";
 		}
-		StaticText scores = new StaticText(0, 0, Color.WHITE, scoreString);
-		scores.setPosition((TowerDefense.getWidth() - scores.getWidth()) / 2, (TowerDefense.getHeight() - scores.getActualHeight()));
+		Label scores = new Label(scoreString, this.game.getLabelStyle());
+		float actualHeight = scores.getHeight() * scores.getText().toString().split("\n").length;
+
+		scores.setPosition((TowerDefense.getWidth() - scores.getWidth()) / 2, (TowerDefense.getHeight() - actualHeight));
 		scores.setColor(Color.BLACK);
-		this.guiElements.add(scores);
+		this.addActor(scores);
 
 	}
 
